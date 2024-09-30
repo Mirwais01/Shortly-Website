c1d53bf8d70631d60d52f4ff049b4df30063102d
Mirwais <mirwais.sanaie@gmail.com>
2024-09-30 21:07:51 +0430

    project completed.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector("#hamburger-menu");
+const munuMobile = document.querySelector("#mobile-menu");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
+  munuMobile.classList.toggle("hidden");
+  munuMobile.classList.toggle("flex");
 });
